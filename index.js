1e1933287f6d219396584db785ec51434ccf3715
Thu Pham <lethu7162@gmail.com>
2022-05-18 13:22:57 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function App(){
+    const[quotes, setQuotes] = react.useState
+    return (
+        <div>
+          Hello World
+        </div>
+    );
+}
+ReactDOM.render(<App/>, document.getElementById('app'))
